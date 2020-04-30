99ce08353bf601573407f8795b7a0266d290d680
Mariel-local <marielk.mya@gmail.com>
2020-04-30 16:10:51 -0400

    configurando heroku4

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static('./color-tool-deploy/dist/'));
+app.use(express.static(__dirname + '/dist/color-tool-deploy'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join('./color-tool-deploy/dist/index.html'));
+  res.sendFile(path.join(__dirname+
+    '/dist/color-tool-deploy/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
