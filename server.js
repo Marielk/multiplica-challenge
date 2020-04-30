e082c3b98df3d3d7485cc36c258ecd52d9f417c2
Mariel-local <marielk.mya@gmail.com>
2020-04-30 16:50:40 -0400

    configurando heroku6

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/color-tool-deploy'));
+app.use(express.static(__dirname + '/dist/multiplica-color-tool'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname+
-    '/dist/color-tool-deploy/index.html'));
+    '/dist/multiplica-color-tool/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
